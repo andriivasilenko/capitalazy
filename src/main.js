8e35cbab374276b470958bc1a3fa61b9cffecc0d
andriivasilenko <andrewvasilen@gmail.com>
2017-02-01 20:54:31 +0200

    Add module jsdom

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
+import jsdom from 'jsdom';
 import Portfolio from './Portfolio';
+
+const url = 'http://www.ux.ua/ru/issue.aspx?code=MSICH';
+
+jsdom.env(
+  url,
+  ['http://code.jquery.com/jquery.js'],
+  (err, window) => {
+    if (err) {
+      throw err;
+    }
+    let instrumentBid = window.$(".pname:contains('Покупка') + ").text();
+    let instrumentName = window.$(".pname:contains('Название') + ").text();
+    console.log(instrumentName + ' ' + instrumentBid);
+  }
+);
