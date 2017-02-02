0f9d46036cb98f6690b08008d116c02436cec67c
andriivasilenko <andrewvasilen@gmail.com>
2017-02-02 21:42:09 +0200

    Add automat

---
+++ bin/capitalazy.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+let command = process.argv[2];
+let flag = process.argv[3];
+
+switch (command) {
+  case 'put':
+    //addition instruments
+    break;
+  case 'update':
+    //update portfolio
+    break;
+  case 'watch':
+    //whatch capital
+    break;
+  case 'income':
+    //income today
+    break;
+  case 'add':
+    //add a few stock
+    break;
+  default:
+    console.log('default');
+}
